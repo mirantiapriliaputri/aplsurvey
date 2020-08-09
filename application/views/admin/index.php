39318709f3a8e3bde017aecd89c63a3f9324cde2
Dewi <59911554+DewiMamluatul@users.noreply.github.com>
2020-08-09 13:23:07 +0700

    Update view admin index.php

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -2,176 +2,100 @@
         <div class="container-fluid">
 
           <!-- Page Heading -->
-          <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+          <h2 class="h3 mb-4 ml-5 text-gray-800"><?= $title; ?>
 
+          </h2>
+          <!-- Button trigger modal -->
+          <button type="button" class="btn btn-primary mb-4 ml-5" data-toggle="modal" data-target="#exampleModal">
+            Cara Penggunaan Aplikasi Survey
+          </button>
           <!-- Content Wrapper -->
 
 
-<!-- Content Row -->
-<div class="row">
+          <!-- Content Row -->
+          <div class="row">
 
-  <!-- Earnings (Monthly) Card Example -->
-  <div class="col-xl-3 col-md-6 mb-4 mr-5">
-    <div class="card border-left-primary shadow h-100 py-2">
-      <div class="card-body">
-        <div class="row no-gutters align-items-center">
-          <div class="col mr-2">
-            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Jumlah Pengguna</div>
-            <div class="h5 mb-0 font-weight-bold text-gray-800">100</div>
-          </div>
-          <div class="col-auto">
-            <i class="far fa-user fa-2x text-gray-300"></i>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 
-  <!-- Earnings (Monthly) Card Example -->
-  <div class="col-xl-3 col-md-6 mb-4 mr-5">
-    <div class="card border-left-success shadow h-100 py-2">
-      <div class="card-body">
-        <div class="row no-gutters align-items-center">
-          <div class="col mr-2">
-            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Jumlah Organisasi Terdaftar
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4 ml-5">
+              <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Jumlah Pengguna</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">100</div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="far fa-user fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="h5 mb-0 font-weight-bold text-gray-800">50</div>
+
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4 ml-5">
+              <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Jumlah Organisasi Terdaftar
+                      </div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">50</div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-users fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+
+            <!-- Pending Requests Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4 ml-5">
+              <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Saran</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-comments fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="col-auto">
-            <i class="fas fa-users fa-2x text-gray-300"></i>
+
+
+          <div class="container-fluid text-center mb-3 mt-5">
+            <img class="col-md-4 mx auto" src="<?= base_url('assets/img/logo.jpg'); ?>" alt="Logo" style="height: 200px;width: auto;margin-bottom: 5px;">
+            <h5 class="h6">
+              <b>Selamat Datang</b> di aplikasi Survey
+            </h5>
           </div>
-        </div>
-      </div>
-    </div>
-  </div>
 
-
-
-  <!-- Pending Requests Card Example -->
-  <div class="col-xl-3 col-md-6 mb-4">
-    <div class="card border-left-warning shadow h-100 py-2">
-      <div class="card-body">
-        <div class="row no-gutters align-items-center">
-          <div class="col mr-2">
-            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Saran</div>
-            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+          <!-- Modal -->
+          <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Cara Penggunaan Aplikasi Survey</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <h6>- hbdschsnshsb</h6>
+                  <h6>- jssh fscjbj</h6>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="col-auto">
-            <i class="fas fa-comments fa-2x text-gray-300"></i>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<!-- Content Row -->
-
-<div class="row">
-
-  <!-- Area Chart -->
-  <div class="col-xl-8 col-lg-7">
-    <div class="card shadow mb-4">
-      <!-- Card Header - Dropdown -->
-      <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-        <h6 class="m-0 font-weight-bold text-primary">Grafik Pengguna</h6>
-        <div class="dropdown no-arrow">
-          <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-          </a>
-          <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-            <div class="dropdown-header">Dropdown Header:</div>
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </div>
-      </div>
-      <!-- Card Body -->
-      <div class="card-body">
-        <div class="chart-area">
-          <canvas id="myAreaChart"></canvas>
-        </div>
-      </div>
-
-    </div>
-  </div>
-</div>
-
-
-
-<!-- Content Row -->
-<div class="row">
-
-  <!-- Content Column -->
-  <div class="col-lg-6 mb-4">
-
-    <!-- Project Card Example -->
-    <div class="card shadow mb-4">
-      <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">Projects</h6>
-      </div>
-      <div class="card-body">
-        <h4 class="small font-weight-bold">1 <span class="float-right">25%</span></h4>
-        <div class="progress mb-4">
-          <div class="progress-bar bg-danger" role="progressbar" style="width: 25%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
-        </div>
-        <h4 class="small font-weight-bold">2<span class="float-right">40%</span></h4>
-        <div class="progress mb-4">
-          <div class="progress-bar bg-warning" role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
-        </div>
-        <h4 class="small font-weight-bold">3<span class="float-right">60%</span></h4>
-        <div class="progress mb-4">
-          <div class="progress-bar" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
-        </div>
-        <h4 class="small font-weight-bold">4<span class="float-right">80%</span></h4>
-        <div class="progress mb-4">
-          <div class="progress-bar bg-info" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-        </div>
-        <h4 class="small font-weight-bold">5 <span class="float-right">99%</span></h4>
-        <div class="progress">
-          <div class="progress-bar bg-success" role="progressbar" style="width: 99%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-
-  <div class="col-lg-6 mb-4">
-
-    <!-- Illustrations -->
-    <div class="card shadow mb-4">
-      <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">About Us</h6>
-      </div>
-      <div class="card-body">
-        <div class="text-center">
-          <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="img/undraw_posting_photo.svg" alt="">
-        </div>
-        <p>Add some quality, svg illustrations to your project courtesy of <a target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a constantly updated collection of
-          beautiful svg images
-          that
-          you can use completely free and without attribution!</p>
-        <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on unDraw
-          &rarr;</a>
-      </div>
-    </div>
-
-
-
-  </div>
-</div>
-
-</div>
-<!-- /.container-fluid -->
-
-</div>
-<!-- End of Main Content -->
-
-        </div>
-        <!-- /.container-fluid -->
-
-      </div>
-      <!-- End of Main Content -->
-
